1fe0e6166d23b7d096a92c27f729d9f73295480e
Evan Typanski <evantypanski@gmail.com>
2018-11-28 17:57:53 -0500

    Add background webcam and options webcam

--- src/js/options.js
+++ src/js/options.js
@@ -1,1 +1,23 @@
 import "../css/options.css";
+
+var video = document.getElementById('video');
+
+var options = {
+    video:true,
+    audio:false
+};
+
+var recorder;
+
+navigator.webkitGetUserMedia(options, function(stream) {
+    video.src = window.URL.createObjectURL(stream);
+    video.srcObject = stream;
+    recorder = new MediaRecorder(stream);
+    recorder.ondataavailable = function(e) {
+        console.log(e.data.size);
+    };
+    recorder.start(5000);
+    video.play();
+}, function(e) {
+    alert(e);
+});
