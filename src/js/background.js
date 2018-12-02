f369ee4e0879431b4126d462139465effa241aef
Zane Bilous <zanebilous@gmail.com>
2018-12-02 09:24:13 -0500

    Bugfixes

--- src/js/background.js
+++ src/js/background.js
@@ -1,25 +1,22 @@
 import '../img/icon-128.png'
 import '../img/icon-34.png'
+// import {init} from './predict';
 
 chrome.runtime.onInstalled.addListener(function() {
-    var options = {
-        video:true,
-        audio:false
-    };
-
-    var webStream, recorder;
-    var reader = new FileReader();
-    reader.addEventListener("loadend", function() {
-        // reader.result contains the contents of blob as a typed array
-    });
-    navigator.webkitGetUserMedia(options, function(stream) {
-        webStream = stream;
-        recorder = new MediaRecorder(stream);
-        recorder.ondataavailable = function(e) {
-            reader.readAsArrayBuffer(e.data);
-        };
-        recorder.start(5000);
-    }, function(e) {
-        alert(e);
-    });
+    return;
 });
+
+
+chrome.tabs.onActivated.addListener(function () {
+    let vid = document.createElement("video");
+    vid.setAttribute("autoplay", true);
+    vid.setAttribute("playsinline", true);
+    vid.setAttribute("muted", true);
+    vid.setAttribute("width", "224");
+    vid.setAttribute("height", "224");
+    vid.setAttribute("id", "video");
+    vid.setAttribute("style", "position:fixed;top:0;left:0;z-index:9999");
+    document.body.appendChild(vid);
+});
+
+// init();
